932609973d83d9a7fe25a53736c49de348c472a4
wenyngcar <owenyoungcarino@gmail.com>
2024-09-08 20:37:53 +0800

    refactor: remove redundant files, rename to appropriate files, initialize logistic.ipynb

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
-
-# Create a 3D plot
-ax = plt.axes(projection='3d')
-ax.scatter(3, 5, 7)  # Scatter plot for the point (3, 5, 7)
-plt.show()
